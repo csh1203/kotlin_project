fef39454574c0d65b388e44ac31d16f342c396e6
hyeg <hyeg0121@naver.com>
2023-08-03 19:27:38 +0900

    [feat] fortune service 작성

--- api/src/main/java/com/willpower/api/service/FortuneService.java
+++ api/src/main/java/com/willpower/api/service/FortuneService.java
@@ -1,4 +1,21 @@
 package com.willpower.api.service;
 
+import com.willpower.api.entity.Fortune;
+import com.willpower.api.repository.FortuneRepository;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Getter
+@Service
 public class FortuneService {
+    private final FortuneRepository fortuneRepository;
+
+    public Fortune getRandomFortune(){
+        // TODO : Fortune 개수만큼 랜덤
+        long randomId = (long) Math.ceil(Math.random() * 3 + 1);
+        return fortuneRepository.findById(randomId)
+                .orElseThrow(() -> new IllegalArgumentException("not found : " + randomId));
+    }
 }
