b9bbb3b0dbdb861d14c0d9ccd46acc99bc8a85bb
hyeg <hyeg0121@naver.com>
2023-08-03 19:26:53 +0900

    [feat] fortune controller 작성

--- api/src/main/java/com/willpower/api/controller/FortuneController.java
+++ api/src/main/java/com/willpower/api/controller/FortuneController.java
@@ -1,4 +1,22 @@
 package com.willpower.api.controller;
 
+import com.willpower.api.dto.FortuneResponse;
+import com.willpower.api.entity.Fortune;
+import com.willpower.api.service.FortuneService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
 public class FortuneController {
+    private final FortuneService fortuneService;
+
+    @GetMapping("/api/fortunes")
+    public ResponseEntity<FortuneResponse> getRandomFortune(){
+        Fortune fortune = fortuneService.getRandomFortune();
+
+        return ResponseEntity.ok().body(new FortuneResponse(fortune));
+    }
 }
