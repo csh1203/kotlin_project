24a605d928b37e53641c2a26896523a82317657d
seohyunCho <shanny06@naver.com>
2023-08-17 19:49:41 +0900

    포푼 쿠키 페이지 생성

---
+++ willpower_app/app/src/main/java/com/example/kotlinproject/FortuneCookie.kt
@@ -0,0 +1,11 @@
+package com.example.kotlinproject
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class FortuneCookie: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.fortune_cookie)
+    }
+}
